42b53031654530c050baea1c1a2410e8a0ed7e72
Seweryn Kimla <sewkim@vp.pl>
2018-07-04 10:54:36 +0200

    Complete functionality

--- client/src/App.js
+++ client/src/App.js
@@ -3,6 +3,7 @@ import BookList from './components/BookList'
 import AddBook from './components/AddBook'
 import ApolloClient from 'apollo-boost'
 import { ApolloProvider } from 'react-apollo'
+import './index.css'
 //Apollo setup
 const client = new ApolloClient({
   uri: 'http://localhost:4000/graphql'
--- client/src/components/BookDetails.jsx
+++ client/src/components/BookDetails.jsx
@@ -2,15 +2,40 @@ import { graphql } from 'react-apollo';
 import React, { Component } from 'react'
 import { getBook } from '../queries/queries'
 
- class BookDetails extends Component {
-  render() {
-    return (
-      <div id="book-details">
-        <p>{this.props.bookId}</p>
-      </div>
-    )
-  }
+class BookDetails extends Component {
+
+    displayBookDetails() {
+        const { book } = this.props.data;
+
+        if (book) {
+            return (
+                <div>
+                    <h2>{book.name}</h2>
+                    <p>Gatunek: {book.genre}</p>
+                </div>
+            )
+        }
+
+    }
+
+    render() {
+        console.log(this.props)
+
+        return (
+            <div id="book-details">
+                <p>{this.displayBookDetails()}</p>
+            </div>
+        )
+    }
 }
 
 
-export default graphql(getBook)(BookDetails);
+export default graphql(getBook, {
+    options: (props) => {
+        return {
+            variables: {
+                id: props.bookId
+            }
+        }
+    }
+})(BookDetails);
